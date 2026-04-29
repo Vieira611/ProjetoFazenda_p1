8e22f845e7f2be7d95133ac328f15c84288328e1
filipefernandes318 <filipefernandes1803@gmail.com>
2026-04-28 21:41:48 -0300

    menu

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-<<<<<<< HEAD
-print("Hello, WOrld")
-      
-        
-=======
+admins = []
+clientes = []
 
->>>>>>> 7d549ca (Create main.py)
+op = -99
+while op != 3:
+    print('-------MENU-------')
+    print('1 - Cadastrar usuário')
+    print('2 - Fazer login')
+    print('3 - Encerrar programa')
+    op = int(input('Digite a opção desejada:\n'))
